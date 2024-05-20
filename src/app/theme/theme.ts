a3967b4d175b24477bdecca5084b1ec28cd7c80e
panutthon <93002896+panutthon@users.noreply.github.com>
2024-05-20 16:23:44 +0700

    add 404 add setting SEO

--- src/app/theme/theme.ts
+++ src/app/theme/theme.ts
@@ -1,34 +1,33 @@
-'use client'
+"use client";
 
-import { Roboto } from 'next/font/google'
-import { createTheme } from '@mui/material/styles'
-import { Colors } from './colors'
+import { Roboto } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+import { Colors } from "./colors";
 
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const roboto = Roboto({
-    weight: ['300', '400', '500', '700'],
-    subsets: ['latin'],
-    display: 'swap'
-})
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const theme = createTheme({
-    palette: {
-        mode: 'light',
-        primary: {
-            main: Colors.primary // Your primary color
-        },
-        secondary: {
-            main: Colors.secondary // Your secondary color
-        }
+  palette: {
+    mode: "light",
+    primary: {
+      main: Colors.primary, // Your primary color
     },
-    typography: {
-        fontFamily: roboto.style.fontFamily
-    }
-})
+    secondary: {
+      main: Colors.secondary, // Your secondary color
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
 
-
-export default theme
+export default theme;
