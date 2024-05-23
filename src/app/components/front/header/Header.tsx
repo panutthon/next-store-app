954376a2c6601cea2a16506de58281d8546f32e6
panutthon <93002896+panutthon@users.noreply.github.com>
2024-05-23 13:14:11 +0700

    animation and Footer

--- src/app/components/front/header/Header.tsx
+++ src/app/components/front/header/Header.tsx
@@ -1,11 +1,113 @@
 'use client'
 
-import React from 'react'
+import { AppBar, Box, Button, IconButton, Container, Theme, Toolbar, styled, useMediaQuery } from '@mui/material'
+import { MenuOutlined as MenuIcon } from '@mui/icons-material'
+import Image from 'next/image'
+import Link from 'next/link'
 
-type Props = {}
+export default function Header() {
 
-export default function Header({}: Props) {
+  // AppBar Styling
+  const AppBarStyled = styled(AppBar)(({ theme }) => ({
+    justifyContent: 'center',
+    [theme.breakpoints.up('lg')]: {
+        minHeight: '80px',
+    },
+    backgroundColor: theme.palette.background.default,
+  }))
+
+  // Toolbar Styling
+  const ToolbarStyled = styled(Toolbar)(({ theme }) => ({
+    width: "100%",
+    paddingLeft: "0 !important",
+    paddingRight: "0 !important",
+    color: theme.palette.text.secondary,
+  }))
+
+  // Button Styling
+  const ButtonStyled = styled(Button)(({ theme }) => ({
+    fontSize: '16px',
+    color: theme.palette.text.secondary,
+  }))
+
+  // Breakpoints
+  const lgUp = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'))
+  const lgDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('lg'))
+
   return (
-    <div>Header</div>
+    <AppBarStyled position='sticky' elevation={8}>
+        <Container maxWidth='lg'>
+            <ToolbarStyled>
+                <Link href='/'>
+                    <Image
+                        src={"/images/logos/dark-logo.svg"}
+                        alt="logo"
+                        height={40}
+                        width={174}
+                        priority
+                    />
+                </Link>
+                <Box flexGrow={1} />
+                {
+                    lgDown ? (
+                        <IconButton 
+                            edge='start'
+                            color='inherit'
+                            aria-label='menu'
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    ) : null
+                }
+                {
+                    lgUp ? (
+                        <>
+                            <ButtonStyled
+                                color='inherit'
+                                href='/'
+                                variant='text'
+                                LinkComponent={Link}
+                            >
+                                Home
+                            </ButtonStyled>
+                            <ButtonStyled
+                                color='inherit'
+                                variant='text'
+                                href='/about'
+                                LinkComponent={Link}
+                            >
+                                About
+                            </ButtonStyled>
+                            <ButtonStyled
+                                color='inherit'
+                                variant='text'
+                                href='/blog'
+                                LinkComponent={Link}
+                            >
+                                Blog
+                            </ButtonStyled>
+                            <ButtonStyled
+                                color='inherit'
+                                href='/contact'
+                                variant='text'
+                                sx={{ marginRight: 2 }}
+                                LinkComponent={Link}
+                            >
+                                Contact
+                            </ButtonStyled>
+                            <Button
+                                color='primary'
+                                variant='contained'
+                                href='/login'
+                                LinkComponent={Link}
+                            >
+                                Login
+                            </Button>
+                        </>
+                    ) : null
+                }               
+            </ToolbarStyled>
+        </Container>
+    </AppBarStyled>
   )
 }
