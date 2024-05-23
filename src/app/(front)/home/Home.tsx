3b779f56ab5048a4307b89cf250ab0e11c7c59a6
panutthon <93002896+panutthon@users.noreply.github.com>
2024-05-23 14:14:04 +0700

    frameworks

--- src/app/(front)/home/Home.tsx
+++ src/app/(front)/home/Home.tsx
@@ -1,10 +1,14 @@
-'use client';
+"use client";
 
 import React from "react";
 import Banner from "@/app/components/front/banner/Banner";
+import Frameworks from "@/app/components/front/frameworks/Frameworks";
 
 export default function HomePage() {
   return (
+    <>
       <Banner />
-  )
+      <Frameworks />
+    </>
+  );
 }
